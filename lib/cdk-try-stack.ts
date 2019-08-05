d6729ab25b1383dc7c271fceb11def277ad69bc2
gnk263 <gnk263@users.noreply.github.com>
2019-08-05 19:48:05 +0900

    update stack

--- lib/cdk-try-stack.ts
+++ lib/cdk-try-stack.ts
@@ -1,3 +1,5 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 import cdk = require('@aws-cdk/core');
 
 export class CdkTryStack extends cdk.Stack {
@@ -5,5 +7,28 @@ export class CdkTryStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Lambda
+    const testLambda = new lambda.Function(this, 'TestLambda', {
+      code: lambda.Code.asset('src/lambda'),
+      handler: 'app.handler',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      functionName: 'cdk-test-function',
+    });
+
+    const api = new apigateway.RestApi(this, 'TestCdkApi', {
+      restApiName: 'TestCdkApi',
+    });
+
+    const integration = new apigateway.LambdaIntegration(testLambda, {
+      proxy: true,
+    });
+
+    const resource = api.root.addResource('test');
+    resource.addMethod('GET', integration);
   }
 }
+
+const app = new cdk.App();
+new CdkTryStack(app, 'CdkTryStack');
+app.synth();
